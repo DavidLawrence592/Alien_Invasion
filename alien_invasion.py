41497ebfe19e8687283a4ec443cb6d0f18f87c24
DavidLawrence592 <David_Lawrence1@baylor.edu>
2021-03-14 13:56:22 -0500

    multiple rows of aliens/move fleet right

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             # redraw the screen during each pass through the loop
@@ -87,13 +88,41 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """update the positions of all aliens in the fleet"""
+        self.aliens.update()
+
         # move the ship to the right
         # self.ship.rect.x += 1
 
     def _create_fleet(self):
         """create a fleet of aliens"""
-        # make an alien
+        # create an alien and find the number of aliens in a row
+        # spacing between each alien is equal to one alien width
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # determine the number of rows of aliens that fit on the screen
+        ship_height = self.ship.rect.height
+        available_space_y = (
+            self.settings.screen_height - (3 * alien_height) - ship_height
+        )
+        number_rows = available_space_y // (2 * alien_height)
+
+        # create full fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """create an alien and place it in the first row"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
