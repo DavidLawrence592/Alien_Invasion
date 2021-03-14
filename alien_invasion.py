de2038358af3d53f02c23e046d6a1d4dab7613a4
DavidLawrence592 <David_Lawrence1@baylor.edu>
2021-03-14 17:07:36 -0500

    displayed ship lives and final product

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,6 +82,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -213,8 +214,9 @@ class AlienInvasion:
         """respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
 
-            # decrement ships left
+            # decrement ships left and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # get rid of any remaining aliens and bullets
             self.aliens.empty()
