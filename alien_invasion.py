4d954365eee408b5682c8afd3a085af9ea822f37
DavidLawrence592 <David_Lawrence1@baylor.edu>
2021-03-14 11:49:03 -0500

    limit ship range and refactoring check events

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,21 +43,27 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
 
-                    # move the ship to the right
-                    self.ship.rect.x += 1
+    def _check_keydown_events(self, event):
+        """Responds to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Responds to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+            # move the ship to the right
+            # self.ship.rect.x += 1
 
     def _update_screen(self):
         """update images on the screen, and flip to the new screen"""
